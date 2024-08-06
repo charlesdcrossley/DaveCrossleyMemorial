2b6efb931e1fb56981f719343094d1f250432ffe
Charles Crossley <charlesdcrossley@gmail.com>
2024-08-05 19:44:04 -0500

    added dad's picture to start

---
+++ dave-crossley-memorial/src/App.js
@@ -0,0 +1,16 @@
+import logo from './dave.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>In Loving Memory of H. David Crossley Jr.</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
